f55bac156b666619f1ceb378b14913a18471c918
paulo <paulo.vescovi@viasoft.com.br>
2019-01-02 17:08:18 -0200

    5546546

--- src/main/kotlin/br/com/bico/bicoserver/usuario/UsuarioController.kt
+++ src/main/kotlin/br/com/bico/bicoserver/usuario/UsuarioController.kt
@@ -1,8 +1,11 @@
 package br.com.bico.bicoserver.usuario
 
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
+import java.util.*
 
 @RequestMapping("/usuario")
 @RestController
@@ -10,6 +13,14 @@ class UsuarioController(var usuarioRepository:UsuarioRepository) {
 
     @GetMapping("/teste")
     fun test(): Usuario {
+
+
+        //pegar usuaro logado
+        val authentication = SecurityContextHolder.getContext().authentication
+        if (authentication != null && authentication.name != null) {
+            println(authentication.name!!)
+        }
+
         val usuario = Usuario()
 
         usuario.nome = "PAULO HENRIQUE VESCOVI"
